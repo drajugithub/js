d2caf5641684c1c253cda332f335777559528eb1
D Raju <140903451+drajugithub@users.noreply.github.com>
2023-10-05 10:16:46 +0000

    variables

---
+++ variable.js
@@ -0,0 +1,19 @@
+const accountId=144553
+let accountemail = "raju@gmail.com"
+var accountpassword = "12345"
+accountCity = "bengaluru"
+let accountState;
+
+// accountId = 2//contant are not chnaged
+
+accountemail = "hchc@gmail.com"
+accountpassword="212121"
+accountCity="jaipur"
+
+/*
+prefrerd not use to var because of issue in SCOPE AND FUCNTIONALE SCOPE
+*/
+
+console.log(accountId);
+console.table([accountId,accountemail,accountpassword,accountCity]);
+
