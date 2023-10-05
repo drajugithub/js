3e60450d2b33794be85fd9641223123ef8e8f9c9
D Raju <140903451+drajugithub@users.noreply.github.com>
2023-10-05 15:56:23 +0000

    conversion od datatypes

---
+++ conversonOparation.js
@@ -0,0 +1,24 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33 ==.> 33
+// "33abc" ==> NaN
+// true --> 1 / falsw ==> 0
+
+let isLoggedaIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedaIn);
+console.log(typeof booleanIsLoggedIn)
+console.log( booleanIsLoggedIn)
+
+
+
+// 1 ==> true
+// 0 ==> false
+//"" ==> false
+//"raju"  ==> true
