8c86f8fc9a025032df293456a6a8cf34eee58066
D Raju <140903451+drajugithub@users.noreply.github.com>
2023-10-05 17:11:42 +0000

    oparations

---
+++ oparation.js
@@ -0,0 +1,35 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2)
+// console.log(2**2)
+
+let str1= "hello"
+let str2 = " raju"
+let str3 = str1+str2
+
+// console.log(str3);
+// console.log("1"+2+2)
+// console.log(2+2+"1")
+// console.log(2+"1"+2)
+// console.log(2+3*5%2)
+
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+
+num2=num2 = num3 = 2+2
+
+let gamecounter = 100
+// gamecounter++
+++gamecounter
+console.log(gamecounter);
+
+
+
+
+
+
