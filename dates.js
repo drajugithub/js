4770b51d9a559b01e1465d1458cc7f5ca04a8387
D Raju <140903451+drajugithub@users.noreply.github.com>
2023-10-07 17:37:07 +0000

    dates

---
+++ dates.js
@@ -0,0 +1,28 @@
+//dates 
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleTimeString ());
+
+// let myCretaeDate = new Date(2023,0,23)
+// let myCretaeDate = new Date(2023,0,23,3,5)
+// let myCretaeDate = new Date("2023-01-14")
+let myCretaeDate = new Date("01-04-2023")
+ 
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+
+// console.log(myCretaeDate.toLocaleString());
+// console.log(myCretaeDate.getTime())
+console.log(Math.floor(Date.now()/1000));
+
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
