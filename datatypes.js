10e4cfd6078553574211d28cbca1544edccde029
D Raju <140903451+drajugithub@users.noreply.github.com>
2024-02-20 11:30:48 +0000

    datatypes

--- datatypes.js
+++ datatypes.js
@@ -1,5 +1,19 @@
  "use strict()";   // treat all js code as newer version
 
- // alert(3+5) // we are using nodejs not browser
+//  alert(3+5) // we are using nodejs not browser
 
- 
+
+
+// primitive data types 
+// number 
+// Int
+// string = ""
+// boolean = true/false
+// null= standalone value is there for null
+// undefined = value not be assigned 
+// symbol = unique
+
+// object 
+
+console.log(typeof("raju"))
+console.log(typeof(null)) // null is a  object in js
